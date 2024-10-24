037ec17da9d5e407a4ed8f93f180c1f1152f0ccf
Akash Ghosh <akashghoshofficial700@gmail.com>
2024-10-24 20:54:32 +0530

    add downloadname on send_file function

--- app.py
+++ app.py
@@ -10,33 +10,31 @@ CORS(app)
 
 @app.route('/remove-bg', methods=['POST'])
 def remove_bg():
-    # Check if a file is included in the request
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
 
     file = request.files['file']
 
-    # Check if the file has a valid filename
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
     try:
-        # Open the image using PIL
         input_image = Image.open(file.stream)
 
-        # Remove the background using rembg
+        # Remove background using rembg
         output_image = remove(input_image)
 
-        # Save the output image to a BytesIO stream to avoid saving it on disk
-        img_io = io.BytesIO
+        # Create a BytesIO object to store the image
+        img_io = io.BytesIO()  # Instantiate BytesIO object
         output_image.save(img_io, format='PNG')
         img_io.seek(0)
 
-        # Send the image file back to the client
-        return send_file(img_io, mimetype='image/png', as_attachment=True)
-    
+        # Send the file back to the client as a PNG image
+        return send_file(img_io, mimetype='image/png', download_name='removed-bg-image.png' ,as_attachment=True)
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
